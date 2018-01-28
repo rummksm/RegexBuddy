6475e9032d9b6b7aabf689810b9bcfe1d939d7b2
Maksim Rumyantsev <rummaksimwork@gmail.com>
2018-01-28 14:59:47 +0500

    Final version

--- RegexBuddy/Form1.cs
+++ RegexBuddy/Form1.cs
@@ -11,7 +11,7 @@ using System.Text.RegularExpressions;
 
 namespace RegexBuddy {
     public partial class Form1 : Form {
-        public Form1() {       
+        public Form1() {
             InitializeComponent();
         }
         enum LastColor {
@@ -28,38 +28,42 @@ namespace RegexBuddy {
             SelectText(tb_InputText);
         }
         private void SelectText(object obj) {
-            tb_InputText.SelectionStart = 0;
-            tb_InputText.SelectionLength = tb_InputText.Text.Length;
-            tb_InputText.SelectionBackColor = Color.White;
-            tb_InputText.SelectionStart = tb_InputText.Text.Length;
-            tb_InputText.SelectionLength = 0;
-            tb_InputText.Focus();
-            string inputExpression = @tb_InputRegularExpression.Text;
-            string inputText = tb_InputText.Text;
-            var regex = new Regex(inputExpression);
-            byte lastColor = (byte)LastColor.Green;
-            foreach (Match match in regex.Matches(inputText)) {
-                if (match.Value != "") {
-                    tb_InputText.SelectionStart = match.Index;
-                    tb_InputText.SelectionLength = match.Length;
-                    if (lastColor == (byte)LastColor.Green) {
-                        tb_InputText.SelectionBackColor = Color.Yellow;
-                        lastColor = (byte)LastColor.Yellow;
-                    }
-                    else {
-                        tb_InputText.SelectionBackColor = Color.Green;
-                        lastColor = (byte)LastColor.Green;
-                    }
-                }
-                tb_InputText.SelectionStart = tb_InputText.Text.Length;
+            try {
+                var cursorPosition = tb_InputText.SelectionStart;
+                tb_InputText.SelectionStart = 0;
+                tb_InputText.SelectionLength = tb_InputText.Text.Length;
+                tb_InputText.SelectionBackColor = Color.White;
+                tb_InputText.SelectionStart = cursorPosition;
                 tb_InputText.SelectionLength = 0;
-                tb_InputText.Focus();
+                string inputExpression = @tb_InputRegularExpression.Text;
+                string inputText = tb_InputText.Text;
+                var regex = new Regex(inputExpression);
+                byte lastColor = (byte)LastColor.Green;
+                foreach (Match match in regex.Matches(inputText)) {
+                    if (match.Value != "") {
+                        tb_InputText.SelectionStart = match.Index;
+                        tb_InputText.SelectionLength = match.Length;
+                        if (lastColor == (byte)LastColor.Green) {
+                            tb_InputText.SelectionBackColor = Color.Yellow;
+                            lastColor = (byte)LastColor.Yellow;
+                        }
+                        else {
+                            tb_InputText.SelectionBackColor = Color.Green;
+                            lastColor = (byte)LastColor.Green;
+                        }
+                    }
+                    tb_InputText.SelectionStart = cursorPosition;
+                    tb_InputText.SelectionLength = 0;
+                }
+                if (obj == tb_InputRegularExpression) {
+                    var curPos = tb_InputRegularExpression.SelectionStart;
+                    tb_InputRegularExpression.SelectionLength = 0;
+                }
             }
-            if (obj == tb_InputRegularExpression) {
-                tb_InputRegularExpression.SelectionStart = tb_InputRegularExpression.Text.Length;
+            catch {
+                var curPos = tb_InputRegularExpression.SelectionStart;
                 tb_InputRegularExpression.SelectionLength = 0;
-                tb_InputRegularExpression.Focus();
-            }          
+            }
         }
 
         private void tb_InputRegularExpression_TextChanged(object sender, EventArgs e) {
